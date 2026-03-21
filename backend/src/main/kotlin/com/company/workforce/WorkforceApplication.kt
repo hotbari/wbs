65f3284767b21837395be464cf3b71c219481b02
hotbari <138793425+hotbari@users.noreply.github.com>
2026-03-21 20:30:33 +0900

    feat: backend project setup

---
+++ backend/src/main/kotlin/com/company/workforce/WorkforceApplication.kt
@@ -0,0 +1,11 @@
+package com.company.workforce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WorkforceApplication
+
+fun main(args: Array<String>) {
+    runApplication<WorkforceApplication>(*args)
+}
